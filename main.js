b2225470da3f7dc36d8dd10d28dd78a2614fdebf
bazztube <80954930+powershell-data-download@users.noreply.github.com>
2023-10-12 16:44:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const words = ["My","name","is","Marko","I1","live","in1","the1","Czech","Republic","I2","have","two","siblings","(I","am","the2","middle","child😎)","and1","I3","study","at","I4","like","programming","in2","Java","script","and2","Scratch"]
+
+function link(){
+
+    document.getElementById("zspolackova").src = './Words/Links/Selected/ZŠ Poláčkova.png';
+
+}
+function unlink(){
+
+    document.getElementById("zspolackova").src = './Words/Links/Not selected/ZŠ Poláčkova.png'; 
+
+}
+
+function link1(){
+
+    document.getElementById("W3Schools").src = './Words/Links/Selected/W3Schools.png';
+
+}
+function unlink1(){
+
+    document.getElementById("W3Schools").src = './Words/Links/Not selected/W3Schools.png'; 
+
+}
+
+function link2(){
+
+    document.getElementById("email-link").src = './Words/Links/Selected/email.png';
+
+}
+function unlink2(){
+
+    document.getElementById("email-link").src = './Words/Links/Not selected/email.png'; 
+
+}
+
+function link3(){
+
+    document.getElementById("Instagram").src = './Words/Links/Selected/instagram.png';
+
+}
+function unlink3(){
+
+    document.getElementById("Instagram").src = './Words/Links/Not selected/instagram.png'; 
+
+}
